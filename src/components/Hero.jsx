2a8456bab065423260936779b34dfadf75de219e
Shikhar Negi <shikharnegi31@gmail.com>
2025-11-05 13:59:41 +0530

    made changes in portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 
 const Hero = () => {
-    const resumeLink =
-        "https://drive.google.com/uc?export=download&id=1CVbNOBVwdlDhqKE9SZTNovS4NEuSMq-I";
+    // const resumeLink =
+    //     "https://drive.google.com/uc?export=download&id=1CVbNOBVwdlDhqKE9SZTNovS4NEuSMq-I";
 
     const handleResume = () => {
-        const newTab = window.open(resumeLink, "_blank");
+        const viewLink = "https://drive.google.com/file/d/1CVbNOBVwdlDhqKE9SZTNovS4NEuSMq-I/view?usp=sharing";
+        const downloadLink = "https://drive.google.com/uc?export=download&id=1CVbNOBVwdlDhqKE9SZTNovS4NEuSMq-I";
+
+        window.open(viewLink, "_blank");
 
         const link = document.createElement("a");
-        link.href = resumeLink;
+        link.href = downloadLink;
         link.download = "Shikhar_Negi_Resume.pdf";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
-
-        if (newTab) newTab.focus();
     };
 
     return (
